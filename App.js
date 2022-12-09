a1b8199a33a54b7b911a2c3a55230285e5d23ff0
codiku-dev <codiku.dev@gmail.com>
2022-12-09 13:13:01 +0100

    wip

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Greetings } from "./components/Greetings/Greetings";
+import { View } from "react-native";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Bien joué React Native killer !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Greetings />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
