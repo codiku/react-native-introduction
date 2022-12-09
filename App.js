c470811ee2b8de7c8867ccdbcaae1c158ee1065d
codiku-dev <codiku.dev@gmail.com>
2022-12-09 13:43:13 +0100

    w

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
+import { Image, Text } from "react-native";
+
 import { Greetings } from "./components/Greetings/Greetings";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView>
       <Text>Bienvenue</Text>
-      <Greetings />
+      <Greetings
+        name={"Codiku"}
+        age={30}
+        superFonction={function () {
+          console.log("Yo !");
+        }}
+        isHappy
+        dog={{ name: "Bobun", color: "blanc" }}
+      >
+        <Text>J'espère que ce cours va te plaire !</Text>
+      </Greetings>
     </SafeAreaView>
   );
 }
--- components/Greetings/Greetings.jsx
+++ components/Greetings/Greetings.jsx
@@ -1,9 +1,12 @@
 import { Text } from 'react-native';
 
-export function Greetings() {
+export function Greetings({ name, age, superFonction, isHappy, dog, children }) {
+    superFonction()
+    console.log(" Content ? ", isHappy)
+
     return <>
-        <Text>Sur </Text>
-        <Text>React Native !</Text>
-        <Text>Avec Codiku</Text>
+        <Text>Mon cher {name} tu as {age} ans </Text>
+        <Text>Ton chien c'est {dog.name} il est {dog.color} </Text>
+        {children}
     </>
 }
