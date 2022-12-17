3d6af12795b63c180006b4e116eb7b76e7c9a500
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-12-17 19:53:10 +0100

    wip

--- App.js
+++ App.js
@@ -1,4 +1,14 @@
-import { Greetings } from "./components/Greetings/Greetings";
+import { Text, View } from "react-native";
+import { Human } from "./components/Human/Human";
+
 export default function App() {
-  return <Greetings />;
+  return (
+    <View>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Human />
+      <Text>Hello</Text>
+    </View>
+  );
 }
---
+++ components/Human/Human.jsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export function Human(){
+    return <Text>Je suis un humain</Text>
+}
