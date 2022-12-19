2607ad4311f4291e5a826f58640d767705aa4582
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-12-19 15:16:49 +0100

    wipwip

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
 import { Image, Text } from "react-native";
 
-import { Greetings } from "./components/Greetings/Greetings";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { Human } from "./components/Human/Human";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Bienvenue</Text>
-      <Greetings
-        name={"Codiku"}
-        age={101}
-        likeDog={true}
-        likeCat={true}
-        superFonction={function () {
-          console.log("Yo !");
-        }}
-        isHappy={true}
-        dog={{ name: "Bobun", color: "blanc" }}
-      >
-        <Text>J'espère que ce cours va te plaire !</Text>
-      </Greetings>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Human />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
