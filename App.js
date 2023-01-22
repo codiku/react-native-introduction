d5f079b87fd81d2b531dbaab3d7ced565f5b15bf
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-01-22 18:21:23 +0100

    wip

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import { useState } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Child } from "./components/Child/Child";
-
+import { Platform, Text } from "react-native";
 export default function App() {
-  const [age, setAge] = useState(0);
-
-  function hello(name) {
-    setAge(age + 1);
-  }
-
-  console.log(age);
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <Child onPress={hello} />
+        {Platform.OS === "ios" ? (
+          <Text>Hello sur IOS</Text>
+        ) : (
+          <Text>Hello sur Android</Text>
+        )}
       </SafeAreaView>
     </SafeAreaProvider>
   );
