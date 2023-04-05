398d5cd25e77be1e5bc11f66163e72df42652ed5
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-05 16:15:56 +0200

    wip

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { Text } from "react-native";
-import { Profil } from "./components/Profil/Profil";
-
+import { Text, View } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { s } from "./App.style";
 export default function App() {
   return (
-    <>
-      <Text>Hello !</Text>
-      <Text>Hello !</Text>
-      <Text>Hello !</Text>
-      <Profil />
-      <Text>Hello !</Text>
-    </>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text style={s.title}>Hello !</Text>
+        <View style={s.square}>
+          <Text style={s.innerTxt}>Super!</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
---
+++ App.style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+export const s = StyleSheet.create({
+  title: {
+    color: "red",
+    fontSize: 100,
+    marginTop: 50,
+    marginLeft: "50%",
+  },
+  square: {
+    height: 100,
+    backgroundColor: "green",
+    width: 100,
+  },
+  innerTxt: {
+    color: "white",
+    marginTop: 30,
+    marginLeft: 40,
+  },
+});
