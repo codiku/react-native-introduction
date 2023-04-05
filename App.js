caa10ba8c7f8b8726cb22f601f7e901adeb47664
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-05 10:02:20 +0200

    wip

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from "react-native";
+import { Profil } from "./components/Profil/Profil";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Text>Hello !</Text>
+      <Text>Hello !</Text>
+      <Text>Hello !</Text>
+      <Profil />
+      <Text>Hello !</Text>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Profil/Profil.jsx
@@ -0,0 +1,12 @@
+import { TouchableOpacity, Text } from "react-native";
+
+export function Profil() {
+  return (
+    <>
+      <Text>Hello I am Codiku</Text>
+      <TouchableOpacity>
+        <Text>Click me !</Text>
+      </TouchableOpacity>
+    </>
+  );
+}
