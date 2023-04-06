08213ee7e29ca84cfe5425a66475e836b4dc14db
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-06 16:16:08 +0200

    wip

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { s } from "./App.style";
-import { ProfilCard } from "./components/ProfilCard/ProfilCard";
+import { useState } from "react";
 export default function App() {
+  const [movie, setMovie] = useState("Star wars");
+
+  function updateMovie() {
+    setMovie("Titanic");
+  }
+
+  console.log(movie);
+
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, justifyContent: "center", padding: 20 }}>
-        <ProfilCard />
+      <SafeAreaView style={s.container}>
+        <TouchableOpacity onPress={updateMovie}>
+          <Text style={{ fontSize: 40 }}>{movie}</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- App.style.js
+++ App.style.js
@@ -1,11 +1,9 @@
 import { StyleSheet } from "react-native";
 export const s = StyleSheet.create({
-  box1: {
-    height: 100,
-    width: 100,
-    backgroundColor: "red",
-    alignSelf: "flex-start",
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
   },
-  box2: { height: 100, width: 100, backgroundColor: "orange" },
-  box3: { height: 100, width: 100, backgroundColor: "yellow" },
 });
