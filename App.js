289dae81baa36fe1692ef55e96a37bc7c45bb5c3
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-06 17:21:22 +0200

    wip

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { s } from "./App.style";
-import { ProfilCard } from "./components/ProfilCard/ProfilCard";
-import { Alert, Text } from "react-native";
-import { useState } from "react";
+import { Platform, Text } from "react-native";
 
 export default function App() {
-  const [countClick, setCountClick] = useState(0);
-  function hello(name) {
-    Alert.alert("Hello " + name);
-    setCountClick(countClick + 1);
-  }
   return (
     <SafeAreaProvider>
       <SafeAreaView style={s.container}>
-        <ProfilCard
-          onPressTitle={hello}
-          firstName={"Codiku"}
-          lastName={"TheDev"}
-          age={30}
-        />
-        <Text>You clicked {countClick} times on the title</Text>
+        {Platform.OS === "android" && <Text>Hi on Android</Text>}
+        {Platform.OS === "ios" && <Text>Hi on IOS</Text>}
       </SafeAreaView>
     </SafeAreaProvider>
   );
